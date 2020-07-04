983c5ca48b893f1a66da00aeed789beed85cd2bd
“Name <akrillsm9003@gmail.com>
2020-07-04 14:11:31 +0300

    overflow check

--- js/script.js
+++ js/script.js
@@ -5,61 +5,53 @@ let popupName = document.querySelector(".appointment-name");
 let popupEmail = document.querySelector(".appointment-email");
 let popupText = document.querySelector(".appointment-text")
 let popupForm = document.querySelector(".appointment-form");
-
 let isStorageSupport = true;
 let storage = "";
 let storage2 = "";
-
 try {
-	storage = localStorage.getItem("Name");
-	storage2 = localStorage.getItem("Email");
+storage = localStorage.getItem("Name");
+storage2 = localStorage.getItem("Email");
 } catch(err) {
-	isStorageSupport = false;
+isStorageSupport = false;
 }
-
 btnShow.addEventListener("click", function(evt) {
-	evt.preventDefault();
-	popup.classList.add("popup-show");
-	
-	if (storage && storage2) {
-		popupName.value = storage;
-		popupEmail.value = storage2;
-		popupText.focus();
-	} else {
-		popupName.focus();
-	}
+evt.preventDefault();
+popup.classList.add("popup-show");
+if (storage && storage2) {
+popupName.value = storage;
+popupEmail.value = storage2;
+popupText.focus();
+} else {
+popupName.focus();
+}
 });
-
 btnClose.addEventListener("click", function(evt){
-	evt.preventDefault();
-	popup.classList.remove("popup-show");
-	popup.classList.remove("popup-eror");
+evt.preventDefault();
+popup.classList.remove("popup-show");
+popup.classList.remove("popup-eror");
 });
-
 popupForm.addEventListener("submit", function(evt){
-	if (!popupName.value || !popupEmail.value || !popupText.value) {
-		evt.preventDefault();
-	} else {
-		localStorage.setItem("Name", popupName.value);
-		localStorage.setItem("Email", popupEmail.value);
-	}
+if (!popupName.value || !popupEmail.value || !popupText.value) {
+evt.preventDefault();
+} else {
+localStorage.setItem("Name", popupName.value);
+localStorage.setItem("Email", popupEmail.value);
+}
 });
-
 popupForm.querySelector(".button").addEventListener("click", function(evt){
-	if (!popupName.value || !popupEmail.value || !popupText.value) {
-		evt.preventDefault();
-		popup.classList.remove("popup-eror");
-		popup.offsetWidth = popup.offsetWidth;
-		popup.classList.add("popup-eror");
-	}
+if (!popupName.value || !popupEmail.value || !popupText.value) {
+evt.preventDefault();
+popup.classList.remove("popup-eror");
+popup.offsetWidth = popup.offsetWidth;
+popup.classList.add("popup-eror");
+}
 })
-
 window.addEventListener("keydown", function(evt) {
-	if (evt.keyCode == 27) {
-		if (popup.classList.contains("popup-show")) {
-			evt.preventDefault();
-			popup.classList.remove("popup-show");
-			popup.classList.remove("popup-eror");
-		}
-	}
+if (evt.keyCode == 27) {
+if (popup.classList.contains("popup-show")) {
+evt.preventDefault();
+popup.classList.remove("popup-show");
+popup.classList.remove("popup-eror");
+}
+}
 });
